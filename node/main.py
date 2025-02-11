b8f2e0c9c535e2daabdc5593efd567c81996c056
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-02-11 00:02:39 -0700

    wrote setters for node class

--- node/main.py
+++ node/main.py
@@ -5,6 +5,10 @@ class Node:
     
   def get_value(self):
     return self.value
-
+  
   def get_link_node(self):
     return self.link_node
+  
+  def set_link_node(self, link_node):
+    self.link_node = link_node
+  
