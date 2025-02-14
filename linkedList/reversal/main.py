d66b0f22cbe54d8ae357174308168ae7ab71f987
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-02-14 10:11:43 -0700

    included insert beginning method for LinkedList class

--- linkedList/reversal/main.py
+++ linkedList/reversal/main.py
@@ -15,6 +15,12 @@ class Node:
 class LinkedList:
   def __init__(self, value=None):
     self.head_node = Node(value)
-
+  
   def get_head_node(self):
     return self.head_node
+
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+  
