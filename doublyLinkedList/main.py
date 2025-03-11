69f452b90614cecbbb80b693407e192fd65bb558
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-03-11 10:46:11 -0600

    defined a new DoublyLinkedList.remove_head method

--- doublyLinkedList/main.py
+++ doublyLinkedList/main.py
@@ -41,9 +41,15 @@ class DoublyLinkedList:
   def add_to_tail(self, new_value):
     new_tail = Node(new_value)
     current_tail = self.tail_node
+
     if current_tail != None:
       current_tail.set_next_node(new_tail)
       new_tail.set_prev_node(current_tail)
+
     self.tail_node = new_tail
+
     if self.head_node == None:
       self.head_node = new_tail
+
+def remove_head():
+  pass
