32edcbf47c8117e859adb7cc86615fe9b03668e1
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-03-17 04:57:30 -0600

    defined a new remove_by_value method for the DoublyLinkedList class

--- doublyLinkedList/main.py
+++ doublyLinkedList/main.py
@@ -69,13 +69,19 @@ class DoublyLinkedList:
 
   def remove_tail(self):
     removed_tail = self.tail_node
+
     if removed_tail == None:
       return None
+
     self.tail_node = removed_tail.get_prev_node()
+
     if self.tail_node != None:
       self.tail_node.set_next_node(None)
+
     if removed_tail == self.head_node:
       self.remove_head()
+
     return removed_tail.get_value()
-    
-      
+
+  def remove_by_value(self, value_to_remove):
+    node_to_remove = None
